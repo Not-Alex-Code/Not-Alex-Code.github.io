4fd628b1ae1b5a5764d16905c31e912431348312
KUMATV <KUMATV@LAPTOP-C82971OS>
2022-02-11 18:20:38 -0500

    Fixed Validation

--- Scripts/app.js
+++ Scripts/app.js
@@ -29,25 +29,10 @@
         });
         
 
-            //Step 1 get a reference to an entry point(s) (insertion point / deletion point)
-            //let MainContent = document.getElementsByTagName("main")[0];  
-            //let DocumentBody = document.body;
-            
             //Step 2 create an element(s) to insert
             //let MainParagraph = document.createElement("p");
             let Article = document.createElement("article");
             let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3"> This Is The Article Paragraph </p>`;
-            
-            //Step 3 configure new element
-            //MainParagraph.setAttribute("id", "MainParagraph");
-            //MainParagraph.setAttribute("class", "mt-3");
-
-            // let FirstParagraphString = "This is";
-            // //Example Of Template Strings
-            // let SecondParagraphString = `${FirstParagraphString} The Main Paragraph`;
-
-            // MainParagraph.textContent = SecondParagraphString;
-            // Article.setAttribute("class", "container");
 
             //Step 4 add /  insert element
             //MainContent.appendChild(MainParagraph);
@@ -56,21 +41,6 @@
             $("body").append(`<article class="container">
             <p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>
             </article>`);
-            //DocumentBody.appendChild(Article);
-            
-            
-            //Deletion Example
-            // document.getElementsById("ArticleParagraph").remove();
-
-            //Insert Before Example
-            // let NewH1 = document.createElement("h1");
-            // NewH1.setAttribute("class", "display-1");
-            
-            // MainContent.before(NewH1);
-
-            // NewH1.textContent = "Hello World!";
-
-            
 
     }
     /**
@@ -98,16 +68,16 @@
      * @param {string} error_message 
      */
     
-    function ValidateField(inpue_field_ID, regular_expression, error_message )
+    function ValidateField(input_field_ID, regular_expression, error_message)
     {
         let messageArea = $("#messageArea").hide();
 
-        $("#" + inpue_field_ID).on("blur", function()
+        $("#" + input_field_ID).on("blur", function()
         {
-            let inpue_field_ID = $(this).val();
-            if(!regular_expression.test(inpue_field_ID))
+            let input_text_field = $(this).val();
+            if(!regular_expression.test(input_text_field))
             {
-                $(this).trigger("focus").trigger("select");;
+                $(this).trigger("focus").trigger("select");
                 messageArea.addClass("alert alert-danger").text(error_message).show();
             }
             else
@@ -127,6 +97,7 @@
     function DisplayContactPage()
     {
         console.log("Contact Us Page");
+        
         ContactFormValidation();
 
 
